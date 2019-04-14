29f2b920a89b26da73a7cf7854f1ad79b1510592
DESKTOP-B5GQBSK\kuzuro <kuzuro@gmail.com>
2019-04-15 00:34:40 +0900

    #commit 3
    #3. CRUD 쿼리 및 게시물 목록

--- k_board/src/main/java/com/kboard/dao/BoardDAOImpl.java
+++ k_board/src/main/java/com/kboard/dao/BoardDAOImpl.java
@@ -1,5 +1,7 @@
 package com.kboard.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -15,9 +17,34 @@ public class BoardDAOImpl implements BoardDAO {
 	
 	private static String namespace = "com.kboard.mappers.boardMapper";
 	
+	// 게시물 작성
 	@Override
 	public void write(BoardVO vo) throws Exception {
 		sql.insert(namespace + ".write", vo);
+	}
+
+	// 게시물 수정
+	@Override
+	public void update(BoardVO vo) throws Exception {
+		sql.update(namespace + ".update", vo);
+	}
+
+	// 게시물 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		sql.delete(namespace + ".delete", bno);
+	}
+
+	// 게시물 조회
+	@Override
+	public BoardVO view(int bno) throws Exception {
+		return sql.selectOne(namespace + ".view", bno);
+	}
+
+	// 게시물 목록
+	@Override
+	public List<BoardVO> list() throws Exception {
+		return sql.selectList(namespace + ".list");
 	}
 
 }
--- k_board/src/main/webapp/WEB-INF/views/home.jsp
+++ k_board/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+
+<%-- <%@ page session="false" %> --%>
 
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
@@ -13,5 +14,10 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+
+<p>
+	<a href="/board/list">게시물 목록</a>
+</p>
+
 </body>
 </html>
